6b800d24c7192a3e3dd89c35dc959d9fdad4ea61
diahatsu <diahatsu@users.noreply.github.com>
2018-03-14 11:55:30 -0400

    wrote StackV.cpp

--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,30 @@
 #include "StackV.h"
+#include <vector>
+
+int Stack::size()
+{
+   return data.size();
+}
+
+void Stack::push(int n)
+{
+   data.push_back(n);
+}
+
+void Stack::pop()
+{
+   data.pop_back();
+}
+
+int Stack::top()
+{
+  return data[data.size()-1];
+}
+
+void Stack::clear()
+{
+   for(int i = 0 ; i < data.size() ; i++)
+   {
+   data.pop_back();
+   }
+}
